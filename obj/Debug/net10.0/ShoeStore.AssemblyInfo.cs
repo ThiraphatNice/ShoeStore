66f331f4a7508cce9689d2dd1409e4fe1395441b
ThiraphatNice <thiraphat.san@spumail.net>
2026-03-01 17:32:49 +0700

    Update 01-03-2026-1732

--- obj/Debug/net10.0/ShoeStore.AssemblyInfo.cs
+++ obj/Debug/net10.0/ShoeStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoeStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de46b7628052547a64ab381f0d448e56d65dfda3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+971274d905bf7e9eb75eb9e8e6562d6e8852d544")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoeStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoeStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
